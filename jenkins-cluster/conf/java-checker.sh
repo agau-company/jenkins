8f8a067bc13341f4c46a6079a6b067d95785bbd6
rohithzr <rohithzr@live.com>
2022-04-23 01:34:59 +0530

    add java path to worker.xml

---
+++ jenkins-cluster/conf/java-checker.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+if type -p java; then
+    echo found java executable in PATH
+    _java=java
+elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+    echo found java executable in JAVA_HOME     
+    _java="$JAVA_HOME/bin/java"
+    echo "export JAVA_PATH=$_java" >> ~/.bashrc
+else
+    echo "no java"
+fi
+
+if [[ "$_java" ]]; then
+    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+    echo version "$version"
+    if [[ "$version" != "0" ]]; then
+        echo "export JAVA_VERSION=$version" >> ~/./bashrc
+    else         
+        echo "no java"
+    fi
+fi
